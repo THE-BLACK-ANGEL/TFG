6637c88c5a65411b4401e6ba7b323b43ea2745cc
angel <angelmod948@gmail.com>
2025-10-15 00:24:35 +0200

    Creando Screens para la navegacion que haremos mas tarde

--- app/src/main/java/com/example/tucita/navegacion/Screens.kt
+++ app/src/main/java/com/example/tucita/navegacion/Screens.kt
@@ -2,8 +2,75 @@ package com.example.tucita.navegacion
 
 import kotlinx.serialization.Serializable
 
+//Pantalla login
 @Serializable
 object Login
+//Pantalla de registro
+@Serializable
+object Registro
+//Pantalla de recuperar contraseña
+@Serializable
+object Recuperar
 
+
+
+//Pantalla de Inicio
 @Serializable
 object Inicio
+//Pantalla de servicios disponibles
+@Serializable
+object Servicios
+//Pantalla de mis citas
+@Serializable
+object MisCitas
+//Pantalla de historial de citas
+@Serializable
+object Historial
+//Pantalla de detalle de cita
+@Serializable
+object DetalleCita
+//Pantalla de creaccion de cita
+@Serializable
+object CrearCita
+//Pantalla de confirmacion de cita
+@Serializable
+object Confirmacion
+
+
+
+//Pantalla de perfil
+@Serializable
+object Perfil
+//Pantalla de configuracion de perfil
+@Serializable
+object Configuracion
+//Pantalla de notificaciones
+@Serializable
+object Notificaciones
+
+
+
+//Pantalla de administrador
+@Serializable
+object Administrador
+//Pantalla de manejo de servicios
+@Serializable
+object ManejoServicios
+//Pantalla de manejo de citas
+@Serializable
+object ManejoCitas
+//Pantalla de manejo de usuarios
+@Serializable
+object ManejoUsuarios
+//Pantalla de manejo de profesionales
+@Serializable
+object ManejoProfesionales
+//Pantalla de manejo de roles
+@Serializable
+object ManejoRoles
+//Pantalla de manejo de notificaciones
+@Serializable
+object ManejoNotificaciones
+//Pantalla de manejo de configuracion de Aplicacion
+@Serializable
+object ManejoAplicacion
